3166b90c964417fe17ea59ac9865000352ef47dd
Julia <785533217@qq.com>
2021-02-20 15:52:27 +0800

    执行代码时会先解析引用的包

--- python_practice/work2_objectOriented/XuZhu.py
+++ python_practice/work2_objectOriented/XuZhu.py
@@ -12,8 +12,7 @@ class XuZhu(TongLao):
     def read(self):
         print("罪过罪过！阿弥陀佛~")
 
-
-xz = XuZhu()
+xz = XuZhu()  # 因为xuzhu类引用了tonglao类，所以在子类实例化之前就会解析执行父类的代码
 xz.read()
 xz.fight_zms(500, 50)
 xz.see_people("momo")
